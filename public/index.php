108129897f8899bbae01851f26e851ea344acf53
SoliferNN914 <oskarson16@gmail.com>
2024-12-13 10:36:27 +0200

    added controllers, views and composer. Major refactoring of entire codebase

--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
 <?php
-
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
+use Framework\Router;
 
-$uri = $_SERVER['REQUEST_URI'];
+// instantiating the router
+$router = new Router();
 
-require basePath('router.php');
+// Get routes
+$routes = require basePath('routes.php');
+
+// Get current URI and HTTP method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+
+// Route the request
+$router->route($uri);
